59024c56f1a97e598e4a180e0dffd5af9601ca43
Abel Romero Galvez <mac@Mac-mini-de-Abel.local>
2025-07-04 12:24:55 -0500

    sampleee

--- src/main/java/org/esupportail/desfire/App.java
+++ src/main/java/org/esupportail/desfire/App.java
@@ -7,7 +7,6 @@ import org.slf4j.LoggerFactory;
 
 import javax.smartcardio.CardException;
 
-
 public class App {
 
     private static final Logger log = LoggerFactory.getLogger(App.class);
@@ -15,31 +14,32 @@ public class App {
     private PcscUsbService pcscUsbService = new PcscUsbService();
 
     public static void main(String[] args) {
-        log.info("aaa");
-//        new App().run();
+        System.out.println("aaa");
+        new App().run();
     }
 
-//    public void run(){
-//        DESFireApduService service = new DESFireApduService();
-//
-//        // Get card version
-//        String versionCommand = service.getVersion();
-//        System.out.println("Version APDU: " + versionCommand);
-//
-//        try {
-//            String cardTerminalName = pcscUsbService.connection();
-//            log.debug("cardTerminal : " + cardTerminalName);
-//        } catch (CardException e) {
-//            log.error("pcsc connection error", e);
-//            e.printStackTrace();
-//        }
-//
-////        pcscUsbService.connection();
-//
-//        byte[] rootAid = {0x00, 0x00, 0x00};
-//        String selectCommand = service.selectApplication(rootAid);
-//        System.out.println("Select APDU: " + selectCommand);
-//    }
+    public void run(){
+        log.info("app started");
+        DESFireApduService service = new DESFireApduService();
+
+        // Get card version
+        String versionCommand = service.getVersion();
+        System.out.println("Version APDU: " + versionCommand);
+
+        try {
+            String cardTerminalName = pcscUsbService.connection();
+            log.debug("cardTerminal : " + cardTerminalName);
+        } catch (CardException e) {
+            log.error("pcsc connection error", e);
+            e.printStackTrace();
+        }
+
+//        pcscUsbService.connection();
+
+        byte[] rootAid = {0x00, 0x00, 0x00};
+        String selectCommand = service.selectApplication(rootAid);
+        System.out.println("Select APDU: " + selectCommand);
+    }
 
 
 }
