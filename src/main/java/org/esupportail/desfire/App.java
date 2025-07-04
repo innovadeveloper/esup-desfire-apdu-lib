8ea65773dc2e342187daf705efec03bc9e28ee71
Abel Romero Galvez <mac@Mac-mini-de-Abel.local>
2025-07-04 12:05:06 -0500

    fail

--- src/main/java/org/esupportail/desfire/App.java
+++ src/main/java/org/esupportail/desfire/App.java
@@ -1,8 +1,46 @@
 package org.esupportail.desfire;
 
+import org.esupportail.desfire.service.DESFireApduService;
+import org.esupportail.desfire.service.PcscUsbService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.smartcardio.CardException;
+
+
 public class App {
 
+    private static final Logger log = LoggerFactory.getLogger(App.class);
+
+    private PcscUsbService pcscUsbService = new PcscUsbService();
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        log.info("aaa");
+//        new App().run();
     }
+
+//    public void run(){
+//        DESFireApduService service = new DESFireApduService();
+//
+//        // Get card version
+//        String versionCommand = service.getVersion();
+//        System.out.println("Version APDU: " + versionCommand);
+//
+//        try {
+//            String cardTerminalName = pcscUsbService.connection();
+//            log.debug("cardTerminal : " + cardTerminalName);
+//        } catch (CardException e) {
+//            log.error("pcsc connection error", e);
+//            e.printStackTrace();
+//        }
+//
+////        pcscUsbService.connection();
+//
+//        byte[] rootAid = {0x00, 0x00, 0x00};
+//        String selectCommand = service.selectApplication(rootAid);
+//        System.out.println("Select APDU: " + selectCommand);
+//    }
+
+
 }
+
